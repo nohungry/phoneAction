586e331c590f7be97563a71b34840aac97b5f79f
NoHunGry <tzuyang1991@gmail.com>
2023-04-02 18:06:51 +0800

    feature: phone monitor wakeup down

--- adb_test.py
+++ adb_test.py
@@ -1,36 +1,42 @@
-import subprocess
-
-def getDevice(serial=None):
-    # get device list
-    output = subprocess.check_output(['adb', 'devices'])
-    devices = output.decode().strip().split('\n')[1:]
-
-    # multiple device connect, allows the user to select device
-    if len(devices) > 1:
-        print('Multiple devices connected:')
-        for i, device in enumerate(devices):
-            print(f'{i+1}. {device}')
-        index = int(input('Enter device index: '))
-        device_serial = devices[index-1].split('\t')[0]
-    elif len(devices) == 1:
-        device_serial = devices[0].split('\t')[0]
-    else:
-        print('No device connected')
-        exit()
-
-    # get device message
-    output = subprocess.check_output(['adb', '-s', device_serial, 'shell', 'getprop', 'ro.product.model'])
-    device_model = output.strip().decode()
-
-    print('Device model:', device_model)
-
-    return device_serial
-
-def getSnapShot():
-    screenShot = subprocess.check_output(['adb', 'shell', 'screencap', '-p'])
-    with open("screenshot01.png", "wb") as f:
-        f.write(screenShot)
+# import subprocess
+#
+# def getDevice(serial=None):
+#     # get device list
+#     output = subprocess.check_output(['adb', 'devices'])
+#     devices = output.decode().strip().split('\n')[1:]
+#
+#     # multiple device connect, allows the user to select device
+#     if len(devices) > 1:
+#         print('Multiple devices connected:')
+#         for i, device in enumerate(devices):
+#             print(f'{i+1}. {device}')
+#         index = int(input('Enter device index: '))
+#         device_serial = devices[index-1].split('\t')[0]
+#     elif len(devices) == 1:
+#         device_serial = devices[0].split('\t')[0]
+#     else:
+#         print('No device connected')
+#         exit()
+#
+#     # get device message
+#     output = subprocess.check_output(['adb', '-s', device_serial, 'shell', 'getprop', 'ro.product.model'])
+#     device_model = output.strip().decode()
+#
+#     print('Device model:', device_model)
+#
+#     return device_serial
+#
+# def getSnapShot():
+#     screenShot = subprocess.check_output(['adb', 'shell', 'screencap', '-p'])
+#     with open("screenshot01.png", "wb") as f:
+#         f.write(screenShot)
+#
+# if __name__ == '__main__':
+#     deviceSerial = getDevice()
+#     getSnapShot()
+import os
 
 if __name__ == '__main__':
-    deviceSerial = getDevice()
-    getSnapShot()
+    dirtest = os.getcwd()
+    print(dirtest)
+
--- phoneAction.py
+++ phoneAction.py
@@ -2,6 +2,7 @@ from ppadb.client import Client as AdbClient
 from datetime import datetime
 
 import subprocess
+import os
 import time
 
 def adbWakeUp():
@@ -10,7 +11,12 @@ def adbWakeUp():
     # waiting adb wakeup time
     time.sleep(10)
 
-def getDevice(serial=None):
+def monitorUp(device):
+    device.shell("input keyevent 26")
+    time.sleep(3)
+    device.shell("input keyevent 82")
+
+def connectDevice(serial=None):
     # Default is "127.0.0.1" and 5037
     client = AdbClient(host="127.0.0.1", port=5037)
 
@@ -28,15 +34,29 @@ def getDevice(serial=None):
     else:
         if devices:
             device = devices[0]
-        return 
+        return device
 
 # @TODO 確認下載本地path & 要如何命名下載圖片(maybe+datetime)
-def deviceScreenshot(device, path):
+def screenshot(device, filename):
+    # 獲取當前手機的snapshot
     screenShot = device.screencap()
-     
+    # 獲取當前資料夾位置
+    currentDir = os.getcwd()
+    # 組合成特定名稱的資料夾
+    # e.g. DeepSea Folder
+    filePath = os.path.join(currentDir, filename)
+    # 開始處理時間並添加到到snapshot後面後墜
+    nowTime = datetime.today().strftime("%Y%m%d_%H%M")
+    temp = os.path.join(filePath, r"screen_" + nowTime + ".png")
+    # 開始寫入到本機對應的資料夾
+    with open(temp, "wb")as f:
+        f.write(screenShot)
+
+    return temp
 
 if __name__ == '__main__':
     # serial 'f130295' 裝置序號
     serial = "f130295"
     adbWakeUp()
-    getDevice(serial=serial)
+    device = connectDevice(serial=serial)
+    monitorUp(device)
